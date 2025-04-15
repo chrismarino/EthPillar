2cf6ec0bb4e5ab524766f760bac01bb51d4b2ef6
Chris Marino <christopher.c.marino@gmail.com>
2025-04-15 10:52:49 -0700

    Use public REACT_APP_BEACONCHAIN_URL. Local node does not support all API calls.

--- plugins/launchpad/plugin_launchpad.sh
+++ plugins/launchpad/plugin_launchpad.sh
@@ -60,19 +60,20 @@ function _setEnvVars(){
         ohai "Creating .env for mainnet: cat \"${PLUGIN_PATH}/$MAINNET_ENV_VARS_FILE\" > \"${LAUNCHPAD_SOURCE_DIR}/.env\""
         cat "${PLUGIN_PATH}/$MAINNET_ENV_VARS_FILE" > "${LAUNCHPAD_SOURCE_DIR}/.env"
         HOST=$(hostname -I | awk '{print $1}') # Get the host's IP address
-        REACT_APP_RPC_URL="http://$HOST:8545" # would https://localhost:8545 work?
+        REACT_APP_RPC_URL="http://$HOST:8545" 
         echo "REACT_APP_RPC_URL=$REACT_APP_RPC_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
-        REACT_APP_BEACONCHAIN_URL="http://$HOST:5052" # would https://localhost:8545 work?
-        echo "REACT_APP_BEACONCHAIN_URL=$REACT_APP_BEACONCHAIN_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
+        # REACT_APP_BEACONCHAIN_URL="http://$HOST:5052" 
+        # echo "REACT_APP_BEACONCHAIN_URL=$REACT_APP_BEACONCHAIN_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
       ;;
       Hoodi)
         ohai "Creating .env for testnet: cat \"${PLUGIN_PATH}/$TESTNET_ENV_VARS_FILE\" > \"${LAUNCHPAD_SOURCE_DIR}/.env\""
         cat "${PLUGIN_PATH}/$TESTNET_ENV_VARS_FILE" > "${LAUNCHPAD_SOURCE_DIR}/.env"
         HOST=$(hostname -I | awk '{print $1}') # Get the host's IP address
-        REACT_APP_RPC_URL="http://$HOST:8545" # would https://localhost:8545 work?
+        REACT_APP_RPC_URL="http://$HOST:8545" 
         echo "REACT_APP_RPC_URL=$REACT_APP_RPC_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
-        REACT_APP_BEACONCHAIN_URL="http://$HOST:5052" # would https://localhost:8545 work?
-        echo "REACT_APP_BEACONCHAIN_URL=$REACT_APP_BEACONCHAIN_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
+        # Can't use local node for BEACONCHAIN_URL because it relies on public apis not available on the node
+        # REACT_APP_BEACONCHAIN_URL="http://$HOST:5052" 
+        # echo "REACT_APP_BEACONCHAIN_URL=$REACT_APP_BEACONCHAIN_URL" >> "${LAUNCHPAD_SOURCE_DIR}/.env"
       ;;
         "Network Syncing")
         echo "Network still sycning. Try again later."
